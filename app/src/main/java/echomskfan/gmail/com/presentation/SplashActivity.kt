8a0c84d213010b6f27673a30966dc7d148c0bb2a
Stanislav Sobol <soboli.s.i@sberbank.ru>
2020-04-04 01:07:26 +0300

    Splash animation

--- app/src/main/java/echomskfan/gmail/com/presentation/SplashActivity.kt
+++ app/src/main/java/echomskfan/gmail/com/presentation/SplashActivity.kt
@@ -3,9 +3,12 @@ package echomskfan.gmail.com.presentation
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import androidx.appcompat.app.AppCompatActivity
 import echomskfan.gmail.com.R
 import echomskfan.gmail.com.presentation.main.MainActivity
+import kotlinx.android.synthetic.main.activity_splash.*
 
 /**
  * Splash-screen activity
@@ -20,6 +23,23 @@ class SplashActivity : AppCompatActivity() {
 
     override fun onResume() {
         super.onResume()
+        val anim: Animation = AnimationUtils.loadAnimation(this, R.anim.splash_anim)
+        anim.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationRepeat(p0: Animation?) {
+            }
+
+            override fun onAnimationEnd(p0: Animation?) {
+                startMainActivity()
+            }
+
+            override fun onAnimationStart(p0: Animation?) {
+            }
+        })
+
+        splashActivityLogoImageView.animation = anim
+    }
+
+    private fun startMainActivity() {
         Handler().postDelayed(
             {
                 startActivity(Intent(this, MainActivity::class.java))
@@ -30,6 +50,6 @@ class SplashActivity : AppCompatActivity() {
     }
 
     companion object {
-        const val SPLASH_DELAY_MS = 1000L
+        const val SPLASH_DELAY_MS = 500L
     }
 }
