943fbb4cca7ec28fe13212b03e908bc2ab5e024e
Stanislav Sobol <soboli.s.i@sberbank.ru>
2020-08-18 00:34:37 +0300

    NightModeAwareIconImage

---
+++ widgetsLib/src/main/java/echomskfan/gmail/com/widgetslib/NightModeAwareIconImage.kt
@@ -0,0 +1,22 @@
+package echomskfan.gmail.com.widgetslib
+
+import android.content.Context
+import android.util.AttributeSet
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.content.ContextCompat
+
+class NightModeAwareIconImage @JvmOverloads constructor(
+    context: Context,
+    attrs: AttributeSet? = null,
+    defStyle: Int = 0
+) : androidx.appcompat.widget.AppCompatImageView(context, attrs, defStyle) {
+
+    override fun setImageResource(resId: Int) {
+        super.setImageResource(resId)
+        val nightModeOn =
+            AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES
+        if (nightModeOn) {
+            setColorFilter(ContextCompat.getColor(context, android.R.color.white))
+        }
+    }
+}
